955ad5e17a851c2b3aa79574cdbb28bc638e6d6d
Sohankalburgi <sohankalburgi2004@gmail.com>
2023-10-30 13:58:14 +0530

    weighted Graphs

--- learning/Graphs/weightedGraph.java
+++ learning/Graphs/weightedGraph.java
@@ -25,6 +25,21 @@ public class weightedGraph {
         adj.get(0).get(1).add(2);
         adj.get(0).get(1).add(3);
 
+        // Edges from 1 to 2 with weight 1
+        adj.get(1).add(new ArrayList<>());
+        adj.get(1).get(0).add(2);
+        adj.get(1).get(0).add(1);
+
+        // Edges from 1 to 3 with weight 4
+        adj.get(1).add(new ArrayList<>());
+        adj.get(1).get(1).add(3);
+        adj.get(1).get(1).add(4);
+
+        // Edges from 2 to 3 with weight 5
+        adj.get(2).add(new ArrayList<>());
+        adj.get(2).get(0).add(3);
+        adj.get(2).get(0).add(5);
+
         System.out.println(adj);
 
     }
