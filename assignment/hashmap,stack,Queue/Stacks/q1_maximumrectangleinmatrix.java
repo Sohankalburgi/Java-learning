cbbd8195b5c9e63b21bc27aaff40bd0ba9a4b162
Sohankalburgi <sohankalburgi2004@gmail.com>
2023-11-06 12:54:25 +0530

    stack

--- assignment/hashmap,stack,Queue/Stacks/q1_maximumrectangleinmatrix.java
+++ assignment/hashmap,stack,Queue/Stacks/q1_maximumrectangleinmatrix.java
@@ -1,78 +1,58 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
+import java.util.Stack; 
 
-public class q1_maximumrectangleinmatrix
-{
-    public static int maximalRectangle(int matrix[][])
-    {
-        if(matrix.length==0)
-        {
-            return 0;
-        }
-        int noofrows = matrix.length;
-        int noofcolumn = matrix[0].length;
+class Main {
+    public static int maximalRectangle(int[][] matrix) {
+        if(matrix.length == 0)  return 0;
         int maxArea = 0;
-        int arr[] = new int[noofcolumn];
-        for(int i=0;i<noofrows;i++)
-        {
-            for(int j=0;j<noofcolumn;j++)
-            {
-                arr[j] = matrix[i][j]==1 ? arr[j]+1:0;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[] arr = new int[col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                arr[j] = matrix[i][j] == 1 ? arr[j] + 1 : 0;
             }
+            //treating arr[j] as histogram, solving max area problem there and updating the max area
+            maxArea = Math.max(maxArea, findMaxAreaInHistogram(arr));
         }
-        maxArea = Math.max(maxArea,findAreaInHistogram(arr));
         return maxArea;
     }
-
-    public static int findAreaInHistogram(int []arr)
-    {
+    public static int findMaxAreaInHistogram(int[] arr){
         int len = arr.length;
         int maxArea = 0;
-        int []left = new int [len];
-        int []right = new int [len];
+        int[] left = new int[len];
+        int[] right = new int[len];
         Stack<Integer> stack = new Stack<>();
-        
-        // travel left to right and find the left limit
-        for(int i=0;i<len;i++)
-        {
-            if(stack.isEmpty())
-            {
+        //traversing left to right, finding left limit
+        for(int i=0;i<len;i++){
+            if(stack.isEmpty()){
                 stack.push(i);
                 left[i] = 0;
-            }
-            else{
-                while(!stack.isEmpty() && arr[stack.peek()]>=arr[i])
-                {
+            }else{
+                while(!stack.isEmpty() && arr[stack.peek()] >= arr[i])
                     stack.pop();
-                }
-                left[i] = stack.isEmpty() ? 0:stack.peek()+1;
-                stack.push(i);  
-            }
-        }
-
-        //empty the stack
-        while(!stack.isEmpty())
-        {
-            stack.pop();
-        }
-
-        // traversing right to left finding the right limit
-        for(int i=len-1;i>=0;i--)
-        {
-            if(stack.isEmpty())
-            {
-                stack.push(len-1);
-                right[i] = len -1;
-            }
-            else{
-                while(!stack.isEmpty() && arr[stack.peek()]>=arr[i])
-                {
-                    stack.pop();
-                }
-                right[i] = stack.isEmpty()? len-1: stack.peek()-1;
+                left[i] = stack.isEmpty() ? 0 : stack.peek()+1;
                 stack.push(i);
             }
         }
+        //doing empty to stack
+        while(!stack.isEmpty())
+            stack.pop();
+        
+        //traversing right to left, find right limit
+        for(int i=len-1;i>=0;i--){
+            if(stack.isEmpty()){
+                stack.push(len-1);
+                right[i] = len - 1;
+            }else{
+                while(!stack.isEmpty() && arr[stack.peek()] >= arr[i])
+                    stack.pop();
+                right[i] = stack.isEmpty() ? len-1 : stack.peek()-1;
+                stack.push(i);
+            }
+        }
+        //traversing the array , caculating area
         int[] area = new int[len];
         for(int i=0;i<len;i++){
             area[i] = (right[i] - left[i] + 1) * arr[i];
@@ -80,9 +60,9 @@ public class q1_maximumrectangleinmatrix
         }
         return maxArea;
     }
-    public static void main(String[] args) {
+    
+	public static void main(String[] args){
         int[][] arr = {{0,1,1,0},{0,1,0,0},{0,0,0,1}};
 	    System.out.println("The maximum area is : " + maximalRectangle(arr));
-
-    }
+	}
 }
