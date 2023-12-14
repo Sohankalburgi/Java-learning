ef964e237a9962683897d5a1a22d9cf50ff59753
Sohan <132459516+Sohankalburgi@users.noreply.github.com>
2023-12-14 17:54:54 +0530

    springbasic

---
+++ learning/spring/FirstJavaWeb/src/test/java/com/example/FirstJavaWeb/sohan/FirstJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstJavaWeb.sohan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
